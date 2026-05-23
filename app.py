0d519e882b0d05019661ef28c2a286a05641ab77
MuYi0101 <a0985882002@gmail.com>
2026-05-23 20:20:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -365,11 +365,11 @@ if st.sidebar.button("開始計算最佳路線"):
             if pair in drawn_pairs:
                 # 回程
                 offset = -0.05  # 往基準向量的相反方向偏
-                color = 'blue'
+                color = 'black'
             else:
                 # 去程
                 offset = 0.05   # 往基準向量的相同方向偏
-                color = 'red'
+                color = 'black'
                 drawn_pairs.add(pair)
         
             # 傳入 base_start 和 base_end 來固定垂直向量方向
